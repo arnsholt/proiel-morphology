75bde703461fad863345d1adb787d64907d74cf0
Arne Skjærholt <arne@luggage.local>
2009-10-31 17:19:55 +0100

    Added 3B conjugation.

--- aux/test.pl
+++ aux/test.pl
@@ -3,21 +3,23 @@
 use strict;
 use warnings;
 
-my @vroots = qw/amo moneo rego audio/;
+my @vroots = qw/amo moneo rego capio audio/;
 my @persons = qw/+1S +2S +3S +1P +2P +3P/;
-my @tenses = qw/+Pres+Ind +Pres+Subj +Fut+Ind/;
+my @tenses = qw/+Pres+Ind +Pres+Subj +Fut+Ind +Impf+Ind +Impf+Subj/;
 
 #die "Usage $0 lexc-file\n" if scalar @ARGV < 1;
 #my $lexicon = $ARGV[0];
 
 #print "read lexc $lexicon\n";
-print "apply down\n";
+#print "apply down\n";
 
 for my $v (@vroots) {
     for my $t (@tenses) {
         for my $p (@persons) {
-            print "$v+Verb$t$p+Act\n";
-            print "$v+Verb$t$p+Pass\n";
+            print "echo apply down $v+Verb$t$p+Act\n";
+            print "apply down $v+Verb$t$p+Act\n";
+            print "echo apply down $v+Verb$t$p+Pass\n";
+            print "apply down $v+Verb$t$p+Pass\n";
         }
     }
 }
